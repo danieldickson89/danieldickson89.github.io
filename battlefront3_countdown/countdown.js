e7dcb20e3702dc27ae62503656fed7afcee16b1f
Daniel Dickson <danieldickson@Daniels-MacBook-Pro.local>
2015-09-19 17:40:54 -0600

    Adding BattleFront 3 Countdown

---
+++ battlefront3_countdown/countdown.js
@@ -0,0 +1,44 @@
+var releaseDate = new Date('2015-11-17 00:00:00')
+
+// give time to release in milliseconds
+function getCountdown(releaseDate) {
+	var currentTime = new Date();
+	var countdown = releaseDate - currentTime;
+	return countdown;
+}
+
+function getText(milliseconds) {
+	var totalSeconds = Math.floor(milliseconds / 1000);
+	var secondsPerMinute = 60;
+	var secondsPerHour = 3600;
+	var secondsPerDay = 86400;
+	var secondsPerWeek= 604800;
+
+	var weeks = Math.floor(totalSeconds / secondsPerWeek);
+	totalSeconds -= (weeks * secondsPerWeek);
+	var days = Math.floor(totalSeconds / secondsPerDay);
+	totalSeconds -= (days * secondsPerDay);
+	var hours = Math.floor(totalSeconds / secondsPerHour);
+	totalSeconds -= (hours * secondsPerHour);
+	var minutes = Math.floor(totalSeconds / secondsPerMinute);
+	totalSeconds -= (minutes * secondsPerMinute);
+	var seconds = totalSeconds;
+
+	var weekText = weeks == 1 ? 'week' : 'weeks';
+	var dayText = days == 1 ? 'day' : 'days';
+	var hourText = hours == 1 ? 'hour' : 'hours';
+	var minuteText = minutes == 1 ? 'minute' : 'minutes';
+	var secondText = seconds == 1 ? 'second' : 'seconds';
+
+	var text = [weeks, weekText, days, dayText, hours, hourText, minutes, minuteText, seconds, secondText];
+	return text.join(' ');
+}
+
+function countdown() {
+	var countdown = getCountdown(releaseDate);
+	var text = getText(countdown);
+	document.getElementById('countdown').innerText = text;
+}
+
+countdown();
+setInterval(countdown, 100)
